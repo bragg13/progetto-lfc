4860f566a159f9b4a53d560a6c7e146523862487
bragg13 <bragg003@gmail.com>
2021-10-26 23:24:03 +0200

    Graph init aka input read and graph print

--- main.c
+++ main.c
@@ -3,41 +3,46 @@
 #include <unistd.h>
 #include <string.h>
 #include "set.h"
+#include "subset_construction.h"
 
 int main(int argc, char **argv) {
-    set A, B, C, D;
-    init_set(&A, 2, "A");
-    init_set(&B, 4, "B");
-    init_set(&C, 5, "C");
-    init_set(&D, 5, "D");
+    // set A, B, C, D;          // TODO: replace A, B, C with *A, *B, *C then remove &
+    // init_set(&A, 2, "A");
+    // init_set(&B, 4, "B");
+    // init_set(&C, 5, "C");
+    // init_set(&D, 5, "D");
 
-    // fill A
-    int i;
-    for (i=0; i<2; i++) {
-        node* _node = (node*) malloc(sizeof(node*));
-        create_node(_node, i);
-        set_add_element(&A, _node);
-    }
+    // // fill A
+    // int i;
+    // for (i=0; i<2; i++) {
+    //     node* _node = (node*) malloc(sizeof(node*));
+    //     create_node(_node, i);
+    //     set_add_element(&A, _node);
+    // }
 
-    // fill B
-    for (i=2; i<6; i++) {
-        node* _node = (node*) malloc(sizeof(node*));
-        create_node(_node, i);
-        set_add_element(&B, _node);
-    }
+    // // fill B
+    // for (i=2; i<6; i++) {
+    //     node* _node = (node*) malloc(sizeof(node*));
+    //     create_node(_node, i);
+    //     set_add_element(&B, _node);
+    // }
 
-    // fill C
-    set_union(&C, &A, &B);
+    // // fill C
+    // set_union(&C, &A, &B);
 
-    // fill D
-    set_intersection(&D, &A, &C);
+    // // fill D
+    // set_intersection(&D, &A, &C);
 
+    // set_to_string(&A);
+    // set_to_string(&B);
+    // set_to_string(&C);
+    // set_to_string(&D);
 
-    set_to_string(&A);
-    set_to_string(&B);
-    set_to_string(&C);
-    set_to_string(&D);
+    // printf("\n\n\n");
 
-    printf("\n");
+    Graph nfa;
+    get_input(&nfa);
+    print_nfa(&nfa);
+    
     return 0;
 }
--- subset_construction.c
+++ subset_construction.c
@@ -1,1 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "subset_construction.h"
 
+void get_input(Graph *nfa) {
+    FILE *fp;
+    int states_no;      // number of states
+    int edges_no;       // number of edges
+    int initial_no;     // number of initial states
+    int default_no;     // number of default states
+    int final_no;       // number of final states
+
+
+    // file open
+    fp = fopen("input.txt", "r");
+    if (fp == NULL) {
+        perror("Failed opening file");
+        // return SC_ERROR;
+    }
+
+    // number of states and edges - TODO: could calculate
+    fscanf(fp, "%d %d", &states_no, &edges_no);
+    nfa->states = malloc(sizeof(State)*states_no);    // allocate an array of states ie the nfa
+    nfa->states_no = states_no;
+
+    // number of edges per state + state type
+    int i;
+    int state_edges_no, state_type;
+    for (i=0; i<states_no; i++) {
+        fscanf(fp, "%d %d", &state_edges_no, &state_type);
+
+        nfa->states[i].edges = malloc(sizeof(Edge)*state_edges_no);
+        nfa->states[i].state_id = i;
+        nfa->states[i].state_type = state_type;     // conversion to enum should work
+        nfa->states[i].edges_no = 0;
+    }
+
+    // edges
+    int src, dst, val;
+    for (i=0; i<edges_no; i++) {
+        fscanf(fp, "%d %d %d", &src, &dst, &val);
+        int curr_edge_no = nfa->states[src].edges_no;        // 'cen' ie the first empty edge
+        nfa->states[src].edges[curr_edge_no].src = src;     // get 'cen' edge of node 'src' and set it up
+        nfa->states[src].edges[curr_edge_no].dst = dst;
+        nfa->states[src].edges[curr_edge_no].val = val;
+        nfa->states[src].edges_no++;                         // increment since just set up an edge
+    }
+
+    fclose(fp);
+}
+
+void print_nfa(Graph *nfa) {
+    printf("\n");
+    int i=0;
+    int j=0;
+    for (i=0; i<nfa->states_no; i++) {
+        printf("State: %d\n", i);
+        
+        for (j=0; j<nfa->states[i].edges_no; j++) {
+            Edge e = nfa->states[i].edges[j];
+            printf("\t- %d to %d [%d]\n", e.src, e.dst, e.val);
+        }
+        
+        printf("\n");
+    }
+}
